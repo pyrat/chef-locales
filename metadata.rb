32b04e4e9962cce2d82e89a8947942b507e39328
Alastair Brunton <info@simplyexcited.co.uk>
2011-09-03 20:39:41 +0200

    Implementing the basic locales cookbook.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,12 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "Alastair Brunton"
+maintainer_email "info@simplyexcited.co.uk"
+license          "MIT"
 description      "Installs/Configures locales"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+recipe           "locales - Sets up locales on the server and sets a server time zone."
+
+
+%w{ubuntu debian}.each do |os|
+  supports os
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,29 @@
 # Cookbook Name:: locales
 # Recipe:: default
 #
-# Copyright 2011, YOUR_COMPANY_NAME
+# Copyright 2011, Alastair Brunton
 #
-# All rights reserved - Do Not Redistribute
+# MIT
 #
+
+sudo /usr/sbin/locale-gen en_GB.UTF-8
+sudo /usr/sbin/update-locale LANG=en_GB.UTF-8
+sudo ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+
+execute "Generate the locale" do
+  user "root"
+  command "/usr/sbin/locale-gen #{node[:locales][:server_locale]}"
+end
+
+execute "Set the locale."do
+  user "root"
+  command "/usr/sbin/update-locale LANG=#{node[:locales][:server_locale]}"
+end
+
+
+execute "Set the local time zone for the server." do
+  user "root"
+  command "ln -sf /usr/share/zoneinfo/#{node[:locales][:server_time_zone]} /etc/localtime"
+end
+
+
